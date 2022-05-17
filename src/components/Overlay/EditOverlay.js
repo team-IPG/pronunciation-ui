7850976e95cfa4a18c82997afeaecb5661069cca
George Salinas <george.hanz.salinas@gmail.com>
2022-05-16 18:02:14 -0700

    new updates

--- src/components/Overlay/EditOverlay.js
+++ src/components/Overlay/EditOverlay.js
@@ -43,7 +43,7 @@ export const EditOverlay = (props) => {
       preferredSpeed: +speedRef.current.value,
       active: row.active,
     });
-    setIsSave(true);
+    setIsSave(!isSave);
   };
 
   useEffect(() => {
